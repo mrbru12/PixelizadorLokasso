337e62a852717cfe495e7195dc6ca7ed2125a8f2
Bruno Moretto <bmmcruzalta2@gmail.com>
2020-07-11 18:24:02 -0300

    First commit

---
+++ PixelizadorLokasso/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PixelizadorLokasso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 140);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 288);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(429, 140);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(380, 288);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Gray;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(12, 80);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(797, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.TrackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.ForestGreen;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Impact", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(390, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Black;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(821, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.BackColor = System.Drawing.Color.Khaki;
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salvarComoToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir...";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.AbrirToolStripMenuItem_Click);
+            // 
+            // salvarComoToolStripMenuItem
+            // 
+            this.salvarComoToolStripMenuItem.Name = "salvarComoToolStripMenuItem";
+            this.salvarComoToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.salvarComoToolStripMenuItem.Text = "Salvar Como...";
+            this.salvarComoToolStripMenuItem.Click += new System.EventHandler(this.SalvarComoToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(274, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 36);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pixelizador Lokasso! :)";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveFileDialog1_FileOk);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(580, 25);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(61, 47);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(180, 25);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(61, 47);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(821, 440);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pixelizador Lokasso! :)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarComoToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ PixelizadorLokasso/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+/* {<}================{>} IDEIAS FUTURAS :3 {<}================{>}
+--> ADICIONAR FEATURES NOVAS!!!
+{<}============================================================{>}*/
+
+namespace PixelizadorLokasso
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap rawImgBmp;
+        private Bitmap[] alreadyLoadedBmps;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MaximizeBox = false; 
+            trackBar1.Enabled = false;
+            openFileDialog1.Filter = "Imagem | *.png; *.jpg; *.jpeg; *.bmp;";
+            openFileDialog1.FileName = "";
+            saveFileDialog1.FileName = "Some Daqui";
+            saveFileDialog1.Filter = "Imagem | *.png; *.jpg; *.jpeg; *.bmp;";
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // O MODO ZOOM DEIXA A IMG DO MESMO TAMANHO!!! :3
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox4.BackColor = Color.Transparent;
+        }
+
+        private void AbrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void SalvarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void OpenFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            rawImgBmp = new Bitmap(openFileDialog1.FileName);
+
+            trackBar1.Enabled = true;
+            trackBar1.Value = 1;
+
+            /*
+            int c = 0;
+            for (int i = 1; i < trackBar1.Maximum + 1; i++)
+            {
+                if (rawImgBmp.Width % i == 0 && rawImgBmp.Height % i == 0) { c++; }
+            }
+
+            alreadyLoadedBmps = new Bitmap[c];
+            loadedBmpCounter = 0;
+            */
+
+            alreadyLoadedBmps = new Bitmap[trackBar1.Maximum];
+
+            Bitmap actualBmp = Pixels.NewPixelsBmp(rawImgBmp, trackBar1.Value);
+
+            pictureBox1.Image = rawImgBmp;
+            pictureBox2.Image = actualBmp;
+            alreadyLoadedBmps[0] = actualBmp;
+        }
+
+        private void SaveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox2.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void TrackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+
+            if (rawImgBmp.Width % trackBar1.Value == 0 && rawImgBmp.Height % trackBar1.Value == 0) 
+            {
+                label1.BackColor = Color.ForestGreen;
+
+                if (alreadyLoadedBmps[trackBar1.Value - 1] == null)
+                {
+                    Bitmap bmp = Pixels.NewPixelsBmp(rawImgBmp, trackBar1.Value);
+
+                    pictureBox2.Image = bmp;
+                    alreadyLoadedBmps[trackBar1.Value - 1] = bmp;
+                }
+                else
+                {
+                    pictureBox2.Image = alreadyLoadedBmps[trackBar1.Value - 1];
+                }
+            }
+            else
+            {
+                label1.BackColor = Color.DarkRed;
+            }
+        }
+    }
+
+    static class Pixels
+    {
+        private static SquareVector[] ScaledVectorsMatrix(Size size, int key) 
+        {
+            // int xKey = size.Width / key;
+            // int yKey = size.Height / key;
+            // int biggerKey = (xKey > yKey) ? xKey : yKey;
+            // biggerKey = biggerKey * biggerKey;
+            int biggerKey = (size.Width / key) * (size.Height / key);
+            int i = 0;
+            
+            SquareVector[] matrix = new SquareVector[biggerKey];
+
+            for (int line = 0; line < size.Height; line += key) 
+            {
+                for (int column = 0; column < size.Width; column += key)
+                {
+                    SquareVector vector = new SquareVector() // DA PRA INICIALIZA E SETA AS PROP ASSIM, FICA MAIS BONITO!!! :3
+                    {
+                        NW = new int[] { column, line }, // COLUNA 1° PORQUE ELA REPRESENTA O X E A LINHA O Y!!! :3
+                        // SW = new int[] { column, line + key }, // TIRAR ESSE PRA ECONOMIZAR PROCESSADOR!!! :3
+                        // NE = new int[] { column + key, line }, // ESSE TAMBÉM!!! :3
+                        SE = new int[] { column + key, line + key }
+                    };
+
+                    matrix[i] = vector;
+                    i++;
+                    
+                    // if (i + 1 < biggerKey) { i++; }
+                }
+            }
+            
+            return matrix;
+        }
+
+        public static Bitmap NewPixelsBmp(Bitmap normalBmp, int pixelScale)
+        {
+            SquareVector[] modelMatrix = ScaledVectorsMatrix(normalBmp.Size, pixelScale);
+            Bitmap outputNew = new Bitmap(normalBmp.Size.Width, normalBmp.Size.Height);
+
+            Color[] tempRGBs = new Color[modelMatrix.Length];
+
+            int i = 0;
+
+            foreach (SquareVector vector in modelMatrix)
+            {
+                int[] startVect = vector.NW; 
+                int[] endVect = vector.SE;
+                
+                int c = 0;
+
+                int R = 0;
+                int G = 0;
+                int B = 0;
+
+                for (int line = startVect[1]; line < endVect[1]; line += 1)
+                {
+                    for (int column = startVect[0]; column < endVect[0]; column += 1)
+                    {
+                        Color pxColor = normalBmp.GetPixel(column, line);
+                      
+                        R += pxColor.R;
+                        G += pxColor.G;
+                        B += pxColor.B;
+                        
+                        c++;
+                    }
+                }
+                
+                tempRGBs[i] = Color.FromArgb(R / c, G / c, B / c);
+
+                i++;
+            }
+
+            i = 0;
+
+            foreach (Color color in tempRGBs)
+            {
+                int[] startVect = modelMatrix[i].NW;
+                int[] endVect = modelMatrix[i].SE;
+
+                for (int line = startVect[1]; line < endVect[1]; line += 1)
+                {
+                    for (int column = startVect[0]; column < endVect[0]; column += 1)
+                    {
+                        outputNew.SetPixel(column, line, color);
+                    }
+                }
+
+                i++;
+            }
+
+            return outputNew;    
+        }
+    }
+
+    class SquareVector // AMORZIN DE CLASSE S2!!! :3
+    {
+        private readonly int[][] matrix = new int[2][]; // TAMANHO 4 EM VEZ DE 2 PARA UMA COM QUATRO VECTS!!! :3
+
+        public SquareVector()
+        {
+            for (int i = 0; i < 2; i++) // DE 0 ATÉ 4 EM VEZ DE ATÉ 2 PARA UMA COM QUATRO VECTS!!! :3
+            {
+                matrix[i] = new int[2];
+            }
+        }
+
+        public int[] NW
+        {
+            get { return matrix[0]; }
+            set { matrix[0] = value; }
+        }
+        
+        /*
+        public int[] NE
+        {
+            get { return matrix[1]; }
+            set { matrix[1] = value; }
+        }
+
+        public int[] SW
+        {
+            get { return matrix[2]; }
+            set { matrix[2] = value; }
+        }
+        */
+
+        public int[] SE
+        {
+            get { return matrix[1]; } // 3 EM VEZ DE 1 PARA UMA COM QUATRO VECTS!!! :3
+            set { matrix[1] = value; } // 3 EM VEZ DE 1 PARA UMA COM QUATRO VECTS!!! :3
+        }
+    }
+}
